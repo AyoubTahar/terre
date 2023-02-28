07fa857a0481e592bf9cf527db46f1660c79c942
AyoubTahar <111190719+AyoubTahar@users.noreply.github.com>
2023-02-28 17:39:35 +0100

    slice remplace substring

--- terre03.js
+++ terre03.js
@@ -4,9 +4,10 @@ const readline = require('readline').createInterface({
   })
 const alphabetStr = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
   readline.question('Entrer une lettre \n', lettre => {
-    if (alphabetStr.search(lettre)){
+    if (alphabetStr.search(lettre)&& lettre.length == 1){
         const mini = alphabetStr.replaceAll(' ','')
-        console.log(mini.substring(mini.indexOf(lettre)))
+        const lettreIndex = mini.indexOf(lettre)
+        console.log(mini.slice(lettreIndex))
     }
     else {
         console.log('Entrée non valide')
