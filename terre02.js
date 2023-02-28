4a56ba389e4008de8c11ed7e9fd5430331c19960
AyoubTahar <111190719+AyoubTahar@users.noreply.github.com>
2023-02-28 15:12:59 +0100

    Update terre02.js

--- terre02.js
+++ terre02.js
@@ -1,10 +1,12 @@
-function afficheurArgument(phrase){
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+readline.question('Entrez une phrase \n', phrase => {
     const phraseSansEspace = phrase.split(" ")
     for (mot of phraseSansEspace){
-        console.log(mot)
-
-    }
-}
-
-const test = "Je suis un test"
-afficheurArgument(test)
+    console.log(mot)
+    readline.close();
+  }
+})
