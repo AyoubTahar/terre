e883fd26f5c4e1991a0e422b485f4da92e54a1b1
AyoubTahar <111190719+AyoubTahar@users.noreply.github.com>
2023-03-04 00:16:10 +0100

    Update terre01.js

--- terre01.js
+++ terre01.js
@@ -1,3 +1,9 @@
-const path = __filename
-const filename = path.split('/').pop()
-console.log(filename)
+const path = __filename;
+let filename = "";
+for (let i = path.length - 1; i >= 0; i--) {
+  if (path[i] === "/") {
+    break;
+  }
+  filename = path[i] + filename;
+}
+console.log(filename);
