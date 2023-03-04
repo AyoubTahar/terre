730668ef946933405c84d649c05b5779f9db0563
AyoubTahar <111190719+AyoubTahar@users.noreply.github.com>
2023-03-04 16:05:25 +0100

    utilisation de process.argv

--- terre02.js
+++ terre02.js
@@ -1,12 +1,4 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-readline.question('Entrez une phrase \n', phrase => {
-    const phraseSansEspace = phrase.split(" ")
-    for (mot of phraseSansEspace){
-    console.log(mot)
-    readline.close();
-  }
-})
+const args = process.argv
+for(let i = 2 ; i < args.length; i++){
+  console.log(args[i])
+}
